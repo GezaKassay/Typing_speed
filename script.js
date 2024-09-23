46702362d64b08995da8e63692821021c56e3a47
GezaKassay <kassay_geza@yahoo.com>
2024-09-23 18:12:43 +0300

    progress

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-let noPushSpace = 0;
+const DELAY_TIME = 1000;
+const TEN = 10;
 
-const words = [["color gift eat"], ["color gift eat pain popular average possible tube retain" + 
+const words = [["color gift eat pain popular average possible tube retain" + 
     " digress"], ["incongruous stunning excess revenge banner fortune user " +
     "site harass criminal"], ["snarl facility distribute tray ice speed rib " +
     "section spot bless"], ["resignation stress smell conceive remark charter" +
     " dead hotdog habitat courtship"]]
 
-let displayWords = words[1];
-let text = displayWords[0];
+let displayWords;
+let wordsPos = -1;
+let text;
+
+function changeDisplayWords() {
+    displayWords = words[++wordsPos]; 
+    text = displayWords[0];       
+}
+changeDisplayWords();
+
 let spannedText;
 
 function spanText(text) {   
     let characters = text.split("");
     let num = 0;   
-    spannedText = characters.map(char => `<span id="${++num}">${char}</span>`).join("");
+    spannedText = characters.map(char => `<span id="${++num}">${char}`+
+        `</span>`).join("");
     return spannedText;
 }
 
@@ -34,8 +44,9 @@ function changeToRed() {
 
 let checkPos = 0;
 let letterPos = 0;
+let numWords = 0;
                  
-function checkDisplayedtoTyped() {
+function checkWord() {
     let word = document.getElementById("typeWords").value;         
     for (let i = 0; i <  displayWords.length; ++i) {                      
         for (let j = checkPos; j < displayWords[i].length; ++j) {                                          
@@ -51,16 +62,38 @@ function checkDisplayedtoTyped() {
                 j = displayWords[i].length;
                 ++elmID;                    
             }
-        }
-    }    
+        }        
+    }
+    ++numWords; 
+    if (numWords % TEN === 0) {
+        changeDisplayWords();
+        displaysWords();
+    }     
 }    
 
+let keyPressInstance = 0;
 
-window.addEventListener("keydown", function (checkWord) {         
-    if (checkWord.code === "Space") { 
-        checkDisplayedtoTyped();
-        document.getElementById("typeWords").value = "";       
-    }        
+window.addEventListener("keydown", function (key) {         
+    if (key.code === "Space") { 
+        checkWord();
+        document.getElementById("typeWords").value = "";              
+    }
+    if (keyPressInstance == 0) {
+        startTimer(); 
+        ++keyPressInstance;
+    }       
 });
+
+let time = 60;
+let intervalID;
+
+function decreaseTime() {    
+    --time;
+    document.getElementById("Time").innerHTML = time;    
+}
     
+function startTimer() {
+    document.getElementById("Time").innerHTML = "60";
+    intervalID = setInterval(decreaseTime, DELAY_TIME);    
+}
     
