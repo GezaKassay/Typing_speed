7815dcf788e6dec8df84464aa0ce9aa834919745
GezaKassay <kassay_geza@yahoo.com>
2024-09-24 16:58:49 +0300

    Final mod on checkWord() functionality
    Now the functionality parses correctly the second and so on set of words as well.
    Added functionality to track and display after the 60 sec have passed the number of correctly typed words.

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const words = [["color gift eat pain popular average possible tube retain" +
     " digress"], ["incongruous stunning excess revenge banner fortune user " +
     "site harass criminal"], ["snarl facility distribute tray ice speed rib " +
     "section spot bless"], ["resignation stress smell conceive remark charter" +
-    " dead hotdog habitat courtship"]]
+    " dead hotdog habitat courtship"], ["month pat boom legislature stand face" +
+    " embox experienced urge slot"]];
 
 let displayWords;
 let wordsPos = -1;
@@ -13,15 +14,15 @@ let text;
 
 function changeDisplayWords() {
     displayWords = words[++wordsPos]; 
-    text = displayWords[0];       
+    text = displayWords[0];          
 }
 changeDisplayWords();
 
 let spannedText;
+let num = 0; 
 
 function spanText(text) {   
-    let characters = text.split("");
-    let num = 0;   
+    let characters = text.split("");      
     spannedText = characters.map(char => `<span id="${++num}">${char}`+
         `</span>`).join("");
     return spannedText;
@@ -44,30 +45,43 @@ function changeToRed() {
 
 let checkPos = 0;
 let letterPos = 0;
-let numWords = 0;
+let correctWords = 0;
+let incorrectWords = 0;
                  
 function checkWord() {
-    let word = document.getElementById("typeWords").value;         
-    for (let i = 0; i <  displayWords.length; ++i) {                      
-        for (let j = checkPos; j < displayWords[i].length; ++j) {                                          
+    let word = document.getElementById("typeWords").value;
+    let match = 0;
+    let noMatch = 0;         
+    for (let i = 0; i <  displayWords.length; ++i) {                             
+        for (let j = checkPos; j < displayWords[i].length; ++j) {              
             if (word[letterPos] === displayWords[i][j]) {                                                              
-                changeToGreen();                               
+                changeToGreen();
+                ++match;                               
             } else if (displayWords[i][j] != " ") {                        
-               changeToRed();                           
+               changeToRed();
+               ++noMatch;                                    
             }            
             ++letterPos;
             if (displayWords[i][j] === " ") {
                 checkPos = j + 1;
                 letterPos = 1;   
-                j = displayWords[i].length;
+                j = displayWords[i].length;                    
                 ++elmID;                    
-            }
+            }            
         }        
-    }
-    ++numWords; 
-    if (numWords % TEN === 0) {
+    } 
+    if (noMatch > 0) {
+        ++incorrectWords;
+    } else {
+        ++correctWords;
+    }    
+    match = 0;
+    noMatch = 0; 
+    if ((correctWords + incorrectWords)  % TEN === 0) {
         changeDisplayWords();
         displaysWords();
+        letterPos = 1;
+        checkPos = 0;           
     }     
 }    
 
@@ -75,7 +89,7 @@ let keyPressInstance = 0;
 
 window.addEventListener("keydown", function (key) {         
     if (key.code === "Space") { 
-        checkWord();
+        checkWord();        
         document.getElementById("typeWords").value = "";              
     }
     if (keyPressInstance == 0) {
@@ -89,11 +103,21 @@ let intervalID;
 
 function decreaseTime() {    
     --time;
-    document.getElementById("Time").innerHTML = time;    
+    document.getElementById("Time").innerHTML = time;
+    if (time === 0) {
+        clearInterval(intervalID);
+        document.getElementById("typeWords").readOnly = true; 
+        displayFinalMessage();      
+    }    
 }
     
 function startTimer() {
-    document.getElementById("Time").innerHTML = "60";
+    document.getElementById("Time").innerHTML = time;
     intervalID = setInterval(decreaseTime, DELAY_TIME);    
 }
-    
+
+function displayFinalMessage() {
+    document.getElementById("finalMessage").innerHTML = `You have typed` + 
+        ` ${correctWords} word(s) correctly out of` + 
+        ` ${correctWords + incorrectWords}`;
+}
